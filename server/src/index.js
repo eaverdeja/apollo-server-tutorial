75d5e9653aa185251312c43832d5bd036c1f4e4f
Eduardo Verdeja <eaverdeja@gmail.com>
2018-09-03 21:44:11 -0300

    Apollo Server: Type Relationships

--- server/src/index.js
+++ server/src/index.js
@@ -11,22 +11,47 @@ const schema = gql`
         users: [User!]
         me: User
         user(id: ID!): User
+
+        messages: [Message!]!
+        message(id: ID!): Message!
     }
 
     type User {
         id: ID!
         username: String!
+        messages: [Message!]
+    }
+
+    type Message {
+        id: ID!
+        text: String!
+        user: User!
     }
 `
 
 let users = {
     1: {
         id: '1',
-        username: 'Eduardo Verdeja'
+        username: 'Eduardo Verdeja',
+        messageIds: ['1']
     },
     2: {
         id: '2',
-        username: 'Dave Davids'
+        username: 'Dave Davids',
+        messageIds: ['2']
+    }
+}
+
+let messages = {
+    1: {
+        id: '1',
+        text: 'Hello World',
+        userId: '1'
+    },
+    2: {
+        id: '2',
+        text: 'Bye Cruel World',
+        userId: '2'
     }
 }
 
@@ -35,10 +60,20 @@ const resolvers = {
         users: () => Object.values(users),
         user: (parent, { id }) => users[id],
         me: (parent, args, { me }) => me,
+        messages: () => Object.values(messages),
+        message: (parent, { id }) => messages[id]
     },
 
     User: {
-        username: user => user.username
+        username: user => user.username,
+        messages: user => {
+            return Object.values(messages)
+                .filter(message => message.userId === user.id)
+        }
+    },
+
+    Message: {
+        user: message => users[message.userId]
     }
 }
 
